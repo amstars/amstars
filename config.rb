b10661d9e233b328fb49e5d937caf60d8ce9e2e0
Amy <amstarry@gmail.com>
2013-12-04 10:59:14 -0500

    firstcommit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/amstars/"
+http_images_path = "/amstars/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/amstars/fonts"
+css_dir = "public/amstars/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
